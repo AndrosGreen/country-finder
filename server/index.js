152d0b33c1f5a70999ca3b7d2285d9f54e5a2b48
AndrosGreen <gclandres@gmail.com>
2023-05-29 09:45:07 -0600

    backedn express server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+
+// Initialize the express app
+const app = express();
+// Midleware
+app.use(cors());
+app.use(express.json());
+
+// Make some countries
+import Chance from "chance";
+const chance = new Chance();
+
+const countries = [...Array(250).keys()].map((id) => {
+  return {
+    id,
+    type: chance.country({ full: true }),
+    age: chance.address(),
+  };
+});
+
+// Endpoint to search for animals.
+app.get("", (req, res) => {
+  // Filter results by query
+  const q = req.query.q?.toLowerCase() || "";
+  const results = countries.filter((country) =>
+    country.type.toLocaleLowerCase().includes(q)
+  );
+
+  res.send(results);
+});
+
+app.listen(8080, () => console.log("Listening on port http://localhost:8080"));
